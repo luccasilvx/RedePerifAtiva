2de56681512d4ac4202068c3fd436776e0e9bd4c
Lucas Freitas <lfreitas@oslodtvm.com>
2026-01-04 14:39:39 -0300

    ajustando formulário, seções e paletas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PerifAtiva';
+
+  form = {
+    nome: '',
+    whatsapp: '',
+    email: '',
+    area: 'Educação e Formação'
+  };
+
+  enviarWhatsapp() {
+    const telefoneDestino = '5585996409567';
+
+    const mensagem = `Olá! Me chamo ${this.form.nome}.\n\nGostaria de ser voluntário na Rede PerifAtiva.\n\nMinha área de interesse: ${this.form.area}.\nMeu contato: ${this.form.whatsapp}\nEmail: ${this.form.email}`;
+
+    const link = `https://wa.me/${telefoneDestino}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(link, '_blank');
+  }
 }
